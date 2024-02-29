3a0eeae3749e79f22bf3f643fe32535f26f119ee
Sbat2000 <zevzok@gmail.com>
2024-02-29 21:59:21 +0800

    Initial Commit

---
+++ Travel Schedule/Travel_ScheduleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Travel_ScheduleApp.swift
+//  Travel Schedule
+//
+//  Created by Aleksandr Garipov on 29.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct Travel_ScheduleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

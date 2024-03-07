9d2c7b52c6bea0419f340db4dda605a6b2e95bd9
Sbat2000 <zevzok@gmail.com>
2024-03-07 21:42:06 +0800

    add stations ation

--- Travel Schedule/ContentView.swift
+++ Travel Schedule/ContentView.swift
@@ -6,16 +6,36 @@
 //
 
 import SwiftUI
+import OpenAPIURLSession
 
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Button("Показать ближайшие станции") {
+                stations()
+            }
         }
         .padding()
+    }
+    
+    //MARK: - Actions
+    
+    func stations() {
+        let client = Client (
+            serverURL: try! Servers.server1(),
+            transport: URLSessionTransport()
+        )
+        
+        let service = NearestStationsService(client: client, apikey: "6000100a-5e89-4cc3-b086-926ee56549df")
+        
+        Task {
+            do {
+                let stations = try await service.getNearestStations(lat: 52.253976, lng: 104.327458, distance: 5)
+                print(stations)
+            } catch {
+                print("Ошибка")
+            }
+        }
     }
 }
 

14d6616274dbc6f13a0ced1a6e7c7cfcd0c9c1ca
Sbat2000 <zevzok@gmail.com>
2024-03-10 19:18:04 +0800

    add carrier info service and nearest city service

---
+++ Travel Schedule/Servicies/CarrierInfoService.swift
@@ -0,0 +1,8 @@
+//
+//  CarrierInfoService.swift
+//  Travel Schedule
+//
+//  Created by Aleksandr Garipov on 10.03.2024.
+//
+
+import Foundation
---
+++ Travel Schedule/Servicies/NearestCityService.swift
@@ -0,0 +1,8 @@
+//
+//  NearesCityService.swift
+//  Travel Schedule
+//
+//  Created by Aleksandr Garipov on 10.03.2024.
+//
+
+import Foundation
